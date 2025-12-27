7414bbea88691fd35f9802da694c712dffdd0f09
Natasha Hallberg <Natasha.Hallberg@3shape.com>
2025-12-27 19:10:58 +0100

    train_multiview_vae fully running, with saving pictures

--- visualize.py
+++ visualize.py
@@ -2,6 +2,7 @@ import os
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 import torch
+from torchvision.utils import save_image
 # visualize + find closest (fassn) + T-SNE
 # ----------------------------
 # Visualization
@@ -45,15 +46,17 @@ def visualize_samples_multiview(model, folder, device = torch.device("cuda" if t
         z = torch.randn(num_samples, model.latent_dim).to(device)
         samples = model.decode(z)
         samples = [sample.cpu() for sample in samples]
-
-    for sample in samples:
-        fig, axes = plt.subplots(1, num_samples, figsize=(num_samples * 2, 2))
-        for i in range(num_samples):
-            img = sample[i].permute(1, 2, 0).numpy()
-            axes[i].imshow(img)
-            axes[i].axis('off')
-        plt.tight_layout()
-        plt.savefig(os.path.join(folder, "multiview_vae_samples.png"))
+    batch = torch.cat(samples, dim=0)
+    batch = batch.detach().cpu()
+    # 4 pictures per row
+    save_image(
+        batch, os.path.join(folder, "multiview_vae_samples.png"),
+        nrow=4,
+        padding=10,
+        normalize=False,
+        value_range=(0, 1),
+        pad_value = 1
+    )
 
 def visualize_tsne_multiview(embeddings, caseids, folder):
     os.makedirs(folder, exist_ok=True)
@@ -70,9 +73,5 @@ def visualize_tsne_multiview(embeddings, caseids, folder):
     # Plot
     plt.scatter(latent_2d[:, 0], latent_2d[:, 1], c=colors, cmap='viridis')
     plt.colorbar()
-    plt.title("VAE Latent Space Visualization")
+    plt.title("VAE Latent Space Visualization with t-SNE, yellow is " + unique_labels[1])
     plt.savefig(os.path.join(folder, "multiview_vae_tsne.png"))
-
-def visualize_pca_multiview(embeddings, caseids, folder):
-    os.makedirs(folder, exist_ok=True)
-    embeddings_np = embeddings.numpy()
